c29e728d9c7fcca9c8c93fac1410ec823a92422e
permaicing <jl.5892@pm.me>
2024-09-06 17:09:25 +0000

    Removidas remanescências

--- src/main.py
+++ src/main.py
@@ -3,11 +3,6 @@ import sys
 from time import sleep
 from node import Node
 from utils import parse_int
-"""
-from Data_com import DataCom
-from Server import Server
-from Client import Client
-"""
 
 
 if __name__ == "__main__":
@@ -52,33 +47,3 @@ if __name__ == "__main__":
         elif cmd == 'exit':
             node.dismiss()
             break
-    """
-    numDePares = 2
-    if len(sys.argv) >= 2:
-        numDePares = int(sys.argv[1])
-    
-    info = DataCom("portas.txt", numDePares)
-    servidor = Server(info)
-    cliente = Client(info)
-
-    tserver = threading.Thread(target=servidor.run)
-    tserver.start()
-    sleep(1 / 10)
-
-    print(info)
-    print("**************** [<<ENTER>>=CONECTAR] ****************")
-
-    enter = input().strip() == ''
-    if (enter):
-        print("**************** [<<EXIT>>=SAIR] ****************")
-        tclient = threading.Thread(target=cliente.run)
-        tclient.start()
-        tserver.join()
-        tclient.join()
-        print("**************** FIM CONECTADO ****************")
-        print(input().strip())
-    else:
-        print("**************** ABORT ANTES DE CONECTAR ****************")
-        cliente.close()
-        print(input().strip())
-    """
